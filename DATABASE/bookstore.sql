91083a9c283912fd91ee03569a964729aa87ee16
Nadh1zhere <Nadhir.Trabelsi@fsb.u-carthage.tn>
2020-11-09 16:20:40 +0100

    modification id book ( auto increment )

--- DATABASE/bookstore.sql
+++ DATABASE/bookstore.sql
@@ -1,50 +1,39 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1
--- Généré le : lun. 09 nov. 2020 à 16:08
--- Version du serveur :  10.4.13-MariaDB
--- Version de PHP : 7.4.8
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2020 at 04:18 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Base de données : `bookstore`
+-- Database: `bookstore`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `bookstore`
+-- Table structure for table `bookstore`
 --
 
-CREATE TABLE `bookstore` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `bookstore` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `title` varchar(250) DEFAULT NULL,
   `price` double DEFAULT NULL,
   `author` varchar(250) DEFAULT NULL,
-  `releasedate` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `bookstore`
---
-ALTER TABLE `bookstore`
-  ADD PRIMARY KEY (`id`);
-COMMIT;
+  `releasedate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
