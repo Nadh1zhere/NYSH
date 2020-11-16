e79f86bc2e9314e99d4fbffc44c74e105827ef4a
yassinehamza1996 <yassine.hamza@etudiant-fst.utm.tn>
2020-11-16 14:50:24 +0100

    TAG step 2

---
+++ src/entities/Client.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Client {
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String tel;
+    private String adresse;
+
+    public Client(int id, String nom, String prenom, String email, String tel, String adresse) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.tel = tel;
+        this.adresse = adresse;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", tel=" + tel + ", adresse=" + adresse + '}';
+    }
+
+}
---
+++ src/entities/Commande.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Commande {
+
+    private int id;
+    private Date datecommande;
+    private double prix;
+    private ArrayList<Book> listbooks;
+    private int idC;
+
+    public Commande(int id, Date datecommande, double prix, ArrayList<Book> listbooks, int idC) {
+        this.id = id;
+        this.datecommande = datecommande;
+        this.prix = prix;
+        this.listbooks = listbooks;
+        this.idC = idC;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDatecommande() {
+        return datecommande;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public ArrayList<Book> getListbooks() {
+        return listbooks;
+    }
+
+    public int getIdC() {
+        return idC;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDatecommande(Date datecommande) {
+        this.datecommande = datecommande;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    public void setListbooks(ArrayList<Book> listbooks) {
+        this.listbooks = listbooks;
+    }
+
+    public void setIdC(int idC) {
+        this.idC = idC;
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" + "id=" + id + ", datecommande=" + datecommande + ", prix=" + prix + ", listbooks=" + listbooks + ", idC=" + idC + '}';
+    }
+    
+    
+}
